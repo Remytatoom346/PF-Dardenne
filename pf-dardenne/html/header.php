bc867de09a5ac461c38eb75cf7f103d3a40df34b
remytatoom346 <remydardennevansse@gmail.com>
2023-01-18 15:16:18 +0100

    e

--- pf-dardenne/html/header.php
+++ pf-dardenne/html/header.php
@@ -1,7 +1,6 @@
-
 <div class="container_header">
     <div class="logo_header">
-        <img src="../resources/SVG/Logo_tatoom.svg" alt="logo tatoom créer par rémy dardenne" srcset="" class ='logo'>
+        <img src="../resources/SVG/Logo_tatoom.svg" alt="logo tatoom créer par rémy dardenne" srcset="" class='logo'>
     </div>
     <ul class='container_navbar'>
         <li class="navbar text1"><a href="index.php"> ACCUEIL </a></li>
@@ -9,5 +8,10 @@
         <li class="navbar text3"><a href="a_propos.php">À PROPOS</a></li>
         <li class="navbar text4"><a href="">CONTACT</a></li>
     </ul>
+    <div class="burger_container">
+        <div class="burger"></div>
+        <div class="burger"></div>
+        <div class="burger"></div>
+    </div>
 </div>
 <div class="slider_header">Portfolio | Tatoom - Dardenne Rémy</div>
