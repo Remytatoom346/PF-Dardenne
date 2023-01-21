672aa513438f8e90d52bbbe7b4a8f4bb437104d0
Remytatoom346 <remydardennevansse@gmail.com>
2023-01-21 16:38:28 +0100

    e

--- pf-dardenne/html/header.php
+++ pf-dardenne/html/header.php
@@ -1,23 +1,25 @@
 <div class="container_header">
     <div class="logo_header">
-        <img src="../resources/SVG/Logo_tatoom.svg" alt="logo tatoom créer par rémy dardenne" srcset="" class='logo'>
+        <a href="index.php">
+            <img src="../resources/SVG/Logo_tatoom.svg" alt="logo tatoom créer par rémy dardenne" srcset="" class='logo'>
+        </a>
         <div class="burger_container">
             <div class="burger"></div>
             <div class="burger"></div>
             <div class="burger"></div>
         </div>
         <ul class='container_navbar_mobile'>
-            <li class="navbar_mobile"></li>
-            <li class="navbar_mobile"></li>
-            <li class="navbar_mobile"></li>
-            <li class="navbar_mobile"></li>
+            <li class="navbar_mobile text"><a href="index.php"> ACCUEIL </a></li>
+            <li class="navbar_mobile text"><a href="travaux.php">MES TRAVAUX</a></li>
+            <li class="navbar_mobile text"><a href="a_propos.php">À PROPOS</a></li>
+            <li class="navbar_mobile text"><a href="">CONTACT</a></li>
         </ul>
     </div>
     <ul class='container_navbar'>
-        <li class="navbar text1"><a href="index.php"> ACCUEIL </a></li>
-        <li class="navbar text2"> <a href="travaux.php">MES TRAVAUX </a></li>
-        <li class="navbar text3"><a href="a_propos.php">À PROPOS</a></li>
-        <li class="navbar text4"><a href="">CONTACT</a></li>
+        <li class="navbar text"><a href="index.php"> ACCUEIL </a></li>
+        <li class="navbar text"> <a href="travaux.php">MES TRAVAUX </a></li>
+        <li class="navbar text"><a href="a_propos.php">À PROPOS</a></li>
+        <li class="navbar text"><a href="">CONTACT</a></li>
     </ul>
 
 </div>
--- pf-dardenne/html/index.php
+++ pf-dardenne/html/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="FR">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,9 +10,10 @@
     <link rel="stylesheet" href="../css/reset.css">
     <link rel="stylesheet" href="../css/index.css">
 </head>
+
 <body>
-   <header>
-<?php include 'header.php' ?>
+    <header>
+        <?php include 'header.php' ?>
     </header>
     <!-- DEBUT MAIN -->
     <main>
@@ -20,49 +22,47 @@
                 Bienvenue sur mon Port-folio :
                 <br class='main_text'>
                 <br class='main_text'>
-                Vous pouvez retrouver toutes mes réalisations 
+                Vous pouvez retrouver toutes mes réalisations
                 <br class='main_text'>
-                Mes photographies,  Mes créations graphiques,  Mes sites web, et mes autres projets pro ou personel
+                Mes photographies, Mes créations graphiques, Mes sites web, et mes autres projets pro ou personel
                 <br class='main_text'>
                 <br class='main_text'>
                 Tatoom c'est quoi ? :
                 <br class='main_text'>
-                 C'est mon pseudo, je m'en sert comme nom pour mes créations
+                C'est mon pseudo, je m'en sert comme nom pour mes créations
             </p>
         </div>
-        
+
         <h2 class="titre_card">À Propos</h2>
-        <a href="a_propos.php" >
-            <div class="card_index"></div>
+        <a href="a_propos.php">
+            <img src="../resources/PP.jpg" alt="" srcset="" class="img_propos">
         </a>
-       
+
         <h2 class="titre_card">Mon Travail</h2>
         <a href="travaux.php">
             <div class="card_index"></div>
         </a>
-        
+
 
         <h2 class="titre_card">Mes coordonées</h2>
         <div class="intro">
-            <p>Email :  remydardennevansse@gmail.com 
+            <p>Email : remydardennevansse@gmail.com
                 <br class='main_text'>Tél : 06 95 25 96 34
             </p>
         </div>
         <h2 class="titre_card">Mes résaux</h2>
         <div class="intro">
-            <p class='reseaux_text'> <img src="../resources/SVG/" class='social'>@tatoom346</p>
+            <p class='reseaux_text'> <img src="../resources/SVG/insta.svg" class='social'>@tatoom346</p>
             <p class='reseaux_text'> <img src="../resources/SVG/linkdin.svg" class='social'>Rémy Dardenne</p>
             <p class='reseaux_text'> <img src="../resources/SVG/github.svg" class='social'>Remytatoom346</p>
         </div>
     </main>
     <!-- FIN MAIN -->
     <footer>
-               <?php include 'footer.php' ?>
+        <?php include 'footer.php' ?>
     </footer>
 
 </body>
 <script src="../Js/main.js"></script>
+
 </html>
-
-
-
